c56a23ea7facf5837191095bdb6759b97866299e
takeshi-PC\takeshi <american-tish@mist.ocn.ne.jp>
2017-05-21 22:44:40 +0900

    Bikeをマージ

--- sam/src/sam/Bike.java
+++ sam/src/sam/Bike.java
@@ -2,7 +2,18 @@ package sam;
 
 public class Bike {
 
+	public final String WINDOW = "1";
 	private String num;
+
+	private String one;
+
+	public String getOne() {
+		return one;
+	}
+
+	public void setOne(String one) {
+		this.one = one;
+	}
 
 	public String getNum() {
 		return num;
