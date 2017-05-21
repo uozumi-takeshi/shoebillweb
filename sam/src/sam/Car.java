7715926599f8e59cd0067fd4f1bded2c3dc44998
takeshi-PC\takeshi <american-tish@mist.ocn.ne.jp>
2017-05-21 23:02:03 +0900

    Carにハンドル追加

--- sam/src/sam/Car.java
+++ sam/src/sam/Car.java
@@ -4,6 +4,16 @@ public class Car {
 
 	private String num;
 
+	private String handle;
+
+	public String getHandle() {
+		return handle;
+	}
+
+	public void setHandle(String handle) {
+		this.handle = handle;
+	}
+
 	public String getNum() {
 		return num;
 	}
@@ -11,4 +21,5 @@ public class Car {
 	public void setNum(String num) {
 		this.num = num;
 	}
+
 }
