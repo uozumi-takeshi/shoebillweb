2651104904ad14063c8bb43a1cba4c4e04dd7205
takeshi-PC\takeshi <american-tish@mist.ocn.ne.jp>
2017-05-21 23:13:56 +0900

    Body追加

--- sam/src/sam/Body.java
+++ sam/src/sam/Body.java
@@ -2,4 +2,13 @@ package sam;
 
 public class Body {
 
+	private int newBody;
+
+	public int getNewBody() {
+		return newBody;
+	}
+
+	public void setNewBody(int newBody) {
+		this.newBody = newBody;
+	}
 }
